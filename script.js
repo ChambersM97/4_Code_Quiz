a9bfc7eedbd0ec0a025738ce524a23ccbb3a2ab4
Chambers Mansour <chambersmansour97@gmail.com>
2020-08-07 03:41:25 -0500

    javascript notes

---
+++ script.js
@@ -0,0 +1,19 @@
+const startButton = document.getElementById('start-btn')
+// act like your bringing down from the end of the variable. for example, 
+//startButton is equal to the actually button.
+//so essentially the button is tag with something that goes off
+//if the action being monitored is triggered
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
